30195ae248e58967363d8ff9a4e52e6df8394021
pncl <anormalgecko@gmail.com>
2024-03-25 14:48:37 -0700

    Change sap projectile color again

--- modded_entities.js
+++ modded_entities.js
@@ -592,7 +592,7 @@ class SapSniper extends Enemy {
 
 class SapSniperProjectile extends Entity {
   constructor(pos, angle, radius, speed) {
-    super(pos, radius, "#ad6610");
+    super(pos, radius, "#b89414");
     this.vel.x = Math.cos(angle) * speed;
     this.vel.y = Math.sin(angle) * speed;
     this.Harmless = false;
