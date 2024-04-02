aa172fc54b1d119fc3f62db91a8f4c928941811a
pncl <anormalgecko@gmail.com>
2024-04-02 11:04:34 -0700

    i'm ashamed to commit this

--- modded_heroes.js
+++ modded_heroes.js
@@ -8,10 +8,12 @@ class EnemyEffect {
     this.toRemove = false;
   }
   apply(time, target){
-    this.life -= time;
-    if (this.life < 0){
-      this.toRemove = true;
-      return;
+    if (!this.noDuration){
+      this.life -= time;
+      if (this.life < 0){
+        this.toRemove = true;
+        return;
+      }
     }
     this.doEffect(time, target, this.source);
   }
@@ -20,7 +22,6 @@ class EnemyEffect {
 class JudgementEffect extends EnemyEffect{
   constructor(duration, source){
     super(duration, source, 1000);
-    console.log(duration);
   }
   doEffect(time, target, source){
     target.radiusMultiplier = Math.min((source.radius / target.fixedRadius), 1);
@@ -54,6 +55,155 @@ class Minerva extends Player {
         this.energy -= 20;
         this.firstAbilityCooldown = this.firstTotalCooldown;
       } else {this.aura = true; this.auraType = 5;}
+    }
+  }
+}
+
+class Grom extends Player {
+  constructor(pos, speed) {
+    super(pos, 0, speed, "#8dcc8f", "Grom");
+    this.hasAB = true; this.ab1L = 0; this.ab2L = 0; this.firstTotalCooldown = 8; this.secondTotalCooldown = 0;
+    this.corrodeUses = 0;
+  }
+  abilities(time, area, offset) {
+    if (this.firstAbility && this.firstAbilityCooldown == 0 && this.energy >= 15) {
+      this.corrodeUses++;
+      if (this.corrodeUses >= 2){
+        this.corrodeUses = 0;
+        this.firstAbilityCooldown = this.firstTotalCooldown;
+      }
+      this.energy -= 15;
+      this.spawnBullet(area,'corrode_projectile')
+    }
+  }
+  spawnBullet(area,bulletType){
+    let angle;
+    if(this.mouseActive){
+      angle = this.mouse_angle
+    } else {
+      //FUCKCKKCKKCKCKCKCKCK
+      var directionX = 0;
+      var directionY = 0;
+      if(this.oldPos.x-this.pos.x<0){directionX = 1;}
+      else if(this.oldPos.x-this.pos.x>0){directionX = -1;}
+      if(this.oldPos.y-this.pos.y<0){directionY = 1;}
+      else if(this.oldPos.y-this.pos.y>0){directionY = -1;}
+      angle = Math.atan2(directionY, directionX);
+    }
+    const world = game.worlds[this.world];
+    console.log(world);
+    const bullet = new CorrodeProjectile(new Vector(this.pos.x-world.pos.x-area.pos.x,this.pos.y-world.pos.y-area.pos.y),angle,this.world)
+    if(!area.entities[bulletType]){area.entities[bulletType] = []}
+    area.entities[bulletType].push(bullet);
+  }
+}
+
+class CorrodeProjectile extends Enemy {
+  constructor(pos,angle, world) {
+    super(pos, entityTypes.indexOf("corrode_projectile") - 1, 16/32, undefined, undefined, "#88cc00");
+    this.clock = 0;
+    this.angle = angle;
+    this.no_collide = true;
+    this.imune = true;
+    this.speed = 20;
+    this.outline = false;
+    this.world = world;
+    this.isEnemy = false;
+    this.angleToVel();
+  }
+  behavior(time, area, offset, players) {
+    this.clock += time;
+    if(this.clock>=800){
+      this.toRemove = true;
+    }
+    for(var i in area.entities){
+      const entities = area.entities[i];
+      for(var j in entities){
+        const entity = entities[j];
+        if (distance(this.pos, new Vector(entity.pos.x, entity.pos.y)) < this.radius + entity.radius && entity.isEnemy) {
+          this.toRemove = true;
+          this.spawnBullet(area,'corrode_projectile_exploded')
+          return;
+        }
+      }
+    }
+  }
+  spawnBullet(area,bulletType){
+    //no angle sorry
+    const bullet = new CorrodeProjectileExploded(new Vector(this.pos.x,this.pos.y),0)
+    if(!area.entities[bulletType]){area.entities[bulletType] = []}
+    area.entities[bulletType].push(bullet);
+  }
+  interact(){}
+}
+
+class CorrodeProjectileExploded extends Enemy {
+  constructor(pos,angle) {
+    super(pos, entityTypes.indexOf("corrode_projectile") - 1, 16/32, undefined, undefined, "#88cc0066");
+    this.clock = 0;
+    this.angle = angle;
+    this.no_collide = true;
+    this.imune = true;
+    this.isEnemy = false;
+    this.speed = 0;
+    this.outline = false;
+    this.angleToVel();
+  }
+  behavior(time, area, offset, players) {
+    if (this.fixedRadius < 125/32){
+      this.fixedRadius += 2/32;
+      if (this.fixedRadius > 125/32){
+        this.fixedRadius = 125/32;
+      }
+    }
+    this.clock += time;
+    if(this.clock>=5000){
+      this.toRemove = true;
+    }
+    for(var i in area.entities){
+      const entities = area.entities[i];
+      for(var j in entities){
+        const entity = entities[j];
+        if (distance(this.pos, new Vector(entity.pos.x, entity.pos.y)) < this.radius + entity.radius && !entity.imune) {
+          entity.addEffect(new CorrodeEffect(-1, this))
+        }
+      }
+    }
+  }
+  interact(){}
+}
+
+
+class CorrodeEffect extends EnemyEffect{
+  constructor(duration, source){
+    super(duration, source, 100);
+    this.targets = [];
+    this.targetMultipliers = [];
+  }
+  doEffect(time, target, source){
+    if (source.toRemove){
+      this.toRemove = true;
+    }
+    if (this.targets.indexOf(target) === -1){
+      this.targets.push(target);
+      this.targetMultipliers.push(1);
+    }
+    if (this.targetMultipliers[this.targets.indexOf(target)] !== -1){
+      //this is the most kludge shit i will (hopefully) ever do in my entire life.
+      //one permanent corrode effect is being applied every frame, which probably
+      //sucks for keeping behavior consistent across framerates
+      //and its also stupid. why would i even need to fucking do this?
+      this.targetMultipliers[this.targets.indexOf(target)] -= 0.001 * time / (1000/60);
+      if (target.speedMultiplier <= 0.01){
+        this.targetMultipliers[this.targets.indexOf(target)] === -1;
+        target.HarmlessEffect = 3000;
+        target.Harmless = true;
+        return;
+      }
+      target.speedMultiplier *= this.targetMultipliers[this.targets.indexOf(target)];
+      if (!(distance(source.pos, new Vector(target.pos.x, target.pos.y)) < source.radius + target.radius)) {
+        this.toRemove = true;
+      }
     }
   }
 }
